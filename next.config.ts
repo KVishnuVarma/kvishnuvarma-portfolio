4da3735e5c9ec7fe3150bc1f70bbbdad11040113
KVishnuVarma <raghuvarmakalidindi12345@gmail.com>
2025-09-06 00:50:33 +0530

    configure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/', // all routes point to the homepage
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
